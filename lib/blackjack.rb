e3555070a0e00752dee3759b7d01c9513047e5bc
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 01:04:24 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,10 +35,10 @@ end
 def hit?(card_total)
 prompt_user
 get_user_input
-  if get_user_input = 'h'
+  if get_user_input == 'h'
     card_total += deal_card
     
-  elsif get_user_input = 's'
+  elsif get_user_input == 's'
     return card_total
   
 end
